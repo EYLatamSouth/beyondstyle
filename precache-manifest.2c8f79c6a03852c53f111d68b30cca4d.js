5ea12660e14417cae9740786e3aabdba5492bed9
Eduardo Basso <eduardo.basso@br.ey.com>
2020-09-03 15:42:05 -0300

    Updates

---
+++ precache-manifest.2c8f79c6a03852c53f111d68b30cca4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c141f6fb5e95d29830f643670a531574",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0f98bd0c49a45d0aaf50",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "97af26c7c719b022fbd7",
+    "url": "./static/js/2.0bf3b07f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.0bf3b07f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f98bd0c49a45d0aaf50",
+    "url": "./static/js/main.3bbc332c.chunk.js"
+  },
+  {
+    "revision": "329c03514889575a568a",
+    "url": "./static/js/runtime-main.be2f02e0.js"
+  }
+]);
