9c222b4c7fe588a7c1ca200430608ee5bc43c9cd
brubriele <brunagabriele.paula@gmail.com>
2020-09-10 16:56:04 -0300

    feat: reestrutura makeStyles passando para facilitar controle das classes da biblioteca, define prop types e default prop types e variações de estilo do componente Button

--- example/src/App.js
+++ example/src/App.js
@@ -1,23 +1,40 @@
 import React, { Fragment } from 'react'
 
-import { ProgressBar, PirateButton, Button } from 'beyondstyle';
+import { Button } from 'beyondstyle';
 
 const App = () => {
   const containerStyles = {
     margin: 50
+   
   };
 
   return (
     <Fragment>
       <div style={containerStyles}>
-        <ProgressBar bgcolor="#155CB4" completed={50} />
+        <h1>Primary (disabled | contained | outlined)</h1>
+        <Button variant="contained" color="primary" >Button</Button>
+        <Button variant="outlined" color="primary" >Button</Button>
       </div>
+
       <div style={containerStyles}>
-        <PirateButton variant="contained" />
+        <h1>Secondary (disabled | contained | outlined)</h1>
+        <Button variant="contained" color="secondary" >Button</Button>
+        <Button variant="outlined" color="secondary" >Button</Button>
       </div>
+
       <div style={containerStyles}>
-        <Button variant="contained" />
+        <h1>Tertiary (disabled | contained | outlined)</h1>
+        <Button variant="contained" color="tertiary" >Button</Button>
+        <Button variant="outlined" color="tertiary" >Button</Button>
       </div>
+
+      <div style={containerStyles}>
+        <h1>Disabeld (default | small)</h1>
+        <Button variant="contained" color="tertiary" disabled>Button</Button>
+        <Button variant="contained" color="tertiary" size="small" disabled>Button</Button>
+      </div>
+      
+      
     </Fragment>
   );
 };
--- src/lib/button/index.js
+++ src/lib/button/index.js
@@ -1,28 +1,78 @@
 import React from 'react';
-import { Button } from '@material-ui/core';
+import { Button as MuiButton } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import Layout from '../layout';
+import PropTypes from 'prop-types';
 
-const Component = ({ style }) => {
-  const useStyles = makeStyles((theme) => ({
-    root: {
-      color: theme.palette[style].contrastText,
-      backgroundColor: theme.palette[style].main
-    },
-  }))
 
-  const classes = useStyles();
+const useStyles = makeStyles((theme) => ({
+
+  root: props => (
+    {
+      color: theme.palette[props.style].contrastText,
+      backgroundColor: theme.palette[props.style].main,
+      '&:hover': {
+        background: theme.palette[props.style].dark,
+      },
+      minHeight: theme.spacing(7), // same as 56px
+      paddingRight: theme.spacing(3.75), // 30px
+      paddingLeft: theme.spacing(3.75), // 30px
+    }
+    
+  ),
+  outlined: props => (
+    {
+      color: theme.palette[props.style].contrastText,
+      backgroundColor: `${theme.palette[props.style].background} !important`,
+      '&:hover': {
+        background: theme.palette[props.style].dark,
+      }
+    }
+    
+  ),
+  sizeSmall: props => (
+    {
+      minHeight: theme.spacing(1), // same as 56px
+      paddingRight: theme.spacing(2.25), // 18px
+      paddingLeft: theme.spacing(2.25), // 18px
+      paddingTop: theme.spacing(1.25), // 10x
+      paddingBottom: theme.spacing(1.25), // 10x
+    }
+    
+  )
+  
+}));
+
+const Button = (props) => {
+  const classes = useStyles({style: props.color});
 
   return (
-    <Button className={classes.root} variant="conained"
-    >
-      Button
-    </Button>
+      <MuiButton {...props}
+      classes={{
+        root: classes.root,
+        outlined: classes.outlined,
+        sizeMedium: classes.sizeMedium,
+        sizeLarge: classes.sizeLarge,
+        sizeSmall: classes.sizeSmall,
+      }} >
+        {props.children}
+      </MuiButton>
   );
+};
+
+Button.propTypes = {
+  color: PropTypes.string,
+  children: PropTypes.node.isRequired,
+  disabled: PropTypes.boolean,
+}
+
+Button.defaultProps = {
+  color: 'default',
+  disabled: false,
 };
 
 export default (props) => (
   <Layout>
-    <Component style="secondary" {...props} />
+    <Button {...props}></Button>
   </Layout>
 );
