f1621e954dec9a22c32f8e4d9c389e61fba98fd5
monica mota ocariz <mota.ocariz@gmail.com>
2020-09-10 13:18:58 -0300

    [DSBL-67] Created component FloatingButton

--- example/src/App.js
+++ example/src/App.js
@@ -1,7 +1,8 @@
 import React, { Fragment } from 'react'
 
-import { IconButton } from 'beyondstyle';
+import { IconButton, Fab } from 'beyondstyle';
 import DeleteIcon from '@material-ui/icons/Delete';
+import VisibilityIcon from '@material-ui/icons/Visibility';
 
 const App = () => {
   const containerStyles = {
@@ -60,6 +61,111 @@ const App = () => {
             <DeleteIcon />
           </IconButton>
 
+        </div>
+      </div>
+      <div style={containerStyles}>
+        <h1>Floating button</h1>
+
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Fab aria-label="primary" >
+            <VisibilityIcon />
+          </Fab>
+
+          <Fab aria-label="primary" color="primary">
+            <VisibilityIcon />
+          </Fab>
+
+          <Fab aria-label="secondary" color="secondary">
+            <VisibilityIcon />
+          </Fab>
+
+          <Fab aria-label="tertiary" color="tertiary">
+            <VisibilityIcon />
+          </Fab>
+
+          <Fab aria-label="primary" disabled color="primary">
+            <VisibilityIcon />
+          </Fab>
+        </div>
+      </div>
+      <div style={containerStyles}>
+        <h1>Floating button small</h1>
+
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Fab aria-label="primary" size="small" >
+            <VisibilityIcon />
+          </Fab>
+
+          <Fab aria-label="primary" size="small" color="primary">
+            <VisibilityIcon />
+          </Fab>
+
+          <Fab aria-label="secondary" size="small" color="secondary">
+            <VisibilityIcon />
+          </Fab>
+
+          <Fab aria-label="tertiary" size="small" color="tertiary">
+            <VisibilityIcon />
+          </Fab>
+
+          <Fab aria-label="primary" size="medium" disabled color="primary">
+            <VisibilityIcon />
+          </Fab>
+        </div>
+      </div>
+      <div style={{ width: '50%'}}>
+        <h1>Floating button with text</h1>
+
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Fab aria-label="primary" variant="extended" >
+            <VisibilityIcon />
+            Button
+          </Fab>
+
+          <Fab aria-label="primary" variant="extended" color="primary">
+            <VisibilityIcon />
+            Extended
+          </Fab>
+
+          <Fab aria-label="secondary" variant="extended" color="secondary">
+            <VisibilityIcon />
+            Extended
+          </Fab>
+
+          <Fab aria-label="tertiary" variant="extended" color="tertiary">
+            <VisibilityIcon />
+            Extended
+          </Fab>
+
+          <Fab aria-label="primary" variant="extended" disabled color="primary">
+            <VisibilityIcon />
+            Extended
+          </Fab>
+        </div>
+      </div>
+      <div style={{ width: '50%'}}>
+        <h1>Floating button with text small</h1>
+
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Fab aria-label="primary" variant="extended" size="small" >
+            Button
+          </Fab>
+
+          <Fab aria-label="primary" variant="extended" size="small" color="primary">
+            Button
+          </Fab>
+
+          <Fab aria-label="secondary" variant="extended" size="small" color="secondary">
+            Button
+          </Fab>
+
+          <Fab aria-label="tertiary" variant="extended" size="small" color="tertiary">
+            Button
+          </Fab>
+
+          <Fab aria-label="primary" variant="extended" size="medium" disabled color="primary">
+            Button
+          </Fab>
         </div>
       </div>
     </Fragment>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import IconButton from './lib/icon-button';
+import Fab from './lib/floating-button';
 
 export {
-  IconButton
+  IconButton,
+  Fab
 };
