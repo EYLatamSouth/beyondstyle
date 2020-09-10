9189c836111533f5a902c4819f5951464b83b010
brubriele <brunagabriele.paula@gmail.com>
2020-09-10 20:33:27 -0300

    feat: cria componente Text e importa para aplicação

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import IconButton from './lib/icon-button';
 import Fab from './lib/floating-button';
 import Button from './lib/button';
+import Text from './lib/text';
 
 export {
   IconButton,
   Fab,
-  Button
+  Button,
+  Text
 };
