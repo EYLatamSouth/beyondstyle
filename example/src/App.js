af8dca28600270dbbc504b593e2fae8dbc2b91b3
monica mota ocariz <mota.ocariz@gmail.com>
2020-09-11 08:21:14 -0300

    [DSBL-71] Created style to the variations  Filled Single line

--- example/src/App.js
+++ example/src/App.js
@@ -1,4 +1,8 @@
 import React, { Fragment } from 'react'
+import InputAdornment from '@material-ui/core/InputAdornment';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import IconButton from '@material-ui/core/IconButton';
+import Visibility from '@material-ui/icons/Visibility';
 
 import { TextField } from 'beyondstyle';
 
@@ -10,9 +14,58 @@ const App = () => {
   return (
     <Fragment>
       <div style={containerStyles}>
-        <TextField id="filled-basic" label="Filled" variant="filled" />
+        <TextField disabled id="filled-basic" label="Filled" variant="filled" value='Asd' />
         <hr />
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        <TextField
+          id="input-with-icon-textfield"
+          disabled
+          label="button"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <AccountCircle />
+              </InputAdornment>
+            ),
+          }}
+        />
+        <hr />
+        <TextField
+          id="input-with-icon-textfield"
+          disabled
+          label="2 buttton"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <AccountCircle />
+              </InputAdornment>
+            ),
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  edge="end"
+                >
+                  <Visibility />
+                </IconButton>
+              </InputAdornment>
+            )
+          }}
+        />
+        {/* <hr />
+        <TextField
+          id="standard-multiline-flexible"
+          label="Multiline"
+          multiline
+          rowsMax={4}
+        />
+        <hr />
+        <TextField
+          id="filled-multiline-static"
+          label="Multiline"
+          multiline
+          rows={4}
+          defaultValue="Default Value"
+        /> */}
       </div>
     </Fragment>
   );
--- src/lib/text-field/index.js
+++ src/lib/text-field/index.js
@@ -1,54 +1,151 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from "prop-types"
 import { TextField as Field } from '@material-ui/core';
 import { makeStyles, fade } from '@material-ui/core/styles';
 import Layout from '../layout';
 
 const useStyles = makeStyles((theme) => ({
-  filled: props => ({
-    backgroundColor: theme.palette.grey[100]
+  labelFilledRoot: {
+    color: `${theme.palette.grey[600]} !important`,
+    lineHeight: theme.typography.pxToRem(24),
+    fontSize: theme.typography.pxToRem(16),
+  },
+  labelFilledShrink: {
+    fontSize: theme.typography.pxToRem(12)
+  },
+  labelFilled: props => ({
+    transform: props.startAdornment ? 'translate(46px, 16px) scale(1)' : 'translate(12px, 16px) scale(1)',
+    '&.MuiInputLabel-shrink': {
+      transform: props.startAdornment ? 'translate(46px, 8px) scale(0.75)' : 'translate(12px, 8px) scale(0.75)',
+    },
   }),
-  input: props => ({
-    backgroundColor: theme.palette.grey[100]
-  })
+  labelFilledDisabled: {
+    color: `${fade(theme.palette.primary.main, 0.3)} !important`
+  },
+  filled: props => ({
+    backgroundColor: theme.palette.grey[100],
+    '&:hover': {
+      backgroundColor: theme.palette.grey[100],
+    },
+    '& .MuiInputAdornment-positionStart': {
+      marginTop: `${0} !important`,
+      color: theme.palette.grey[300]
+    },
+    '& .MuiInputAdornment-positionEnd': {
+      color: theme.palette.grey[600],
+      '& .MuiIconButton-root': {
+        color: theme.palette.grey[600],
+      }
+    },
+  }),
+  filledUnderline: props => ({
+    '&:before': {
+      borderColor: theme.palette.grey[300],
+    },
+    '&:hover:before': {
+      borderColor: theme.palette.primary.main,
+    },
+    '&:after': {
+      borderColor: theme.palette.primary.main,
+    },
+  }),
+  filledFocused: {
+    backgroundColor: `${theme.palette.grey[100]} !important`
+  },
+  filledDisabled: {
+    backgroundColor: `${theme.palette.grey[100]} !important`,
+    color: `${fade(theme.palette.primary.main, 0.3)} !important`,
+    '&.MuiFilledInput-underline.Mui-disabled': {
+      '&:before': {
+        borderBottomStyle: 'solid',
+        borderColor: 'transparent',
+      },
+      '&:hover:before': {
+        borderColor: theme.palette.grey[300],
+        borderWidth: 2
+      }
+    }
+  }
 }));
 
 const TextField = (props) => {
-  const classes = useStyles({ color: props.color });
+  const inputRef = React.useRef();
+  const [shrink, setShrink] = React.useState(false);
+  const classes = useStyles({
+    color: props.color,
+    startAdornment: !!props.InputProps?.startAdornment
+  });
 
-  if (props.variant === 'filled') {
+  useEffect(() => {
+    if (hasValue() || props.value) {
+      setShrink(true);
+    }
+  }, []);
+
+  const hasValue = () => {
+    let tag = 'input';
+    if (props.multiline) {
+      tag = 'textarea';
+    }
+
+    return !!inputRef.current.getElementsByTagName(tag)[0].value
+  }
+  const onFocus = () => {
+    setShrink(true);
+  }
+
+  const onBlur = () => {
+    if (!hasValue()) {
+      setShrink(false);
+    }
+  }
+
+  if (props.variant === 'outlined') {
     return (
       <Field
         {...props}
-        InputProps={{
-          classes: {
-            ...classes,
-            root: classes.filled
-          }
-        }}
-        />
+        variant="outlined"
+        classes={{...classes}}
+      />
     )
   }
   return (
     <Field
       {...props}
-      variant="outlined"
-      classes={{...classes}}
+      ref={inputRef}
+      variant="filled"
+      InputLabelProps={{
+        shrink: shrink,
+        classes: {
+          root: classes.labelFilledRoot,
+          filled: classes.labelFilled,
+          shrink: classes.labelFilledShrink,
+          disabled: classes.labelFilledDisabled
+        }
+      }}
+      InputProps={{
+        ...props.InputProps,
+        onFocus: onFocus,
+        onBlur: onBlur,
+        classes: {
+          root: classes.filled,
+          underline: classes.filledUnderline,
+          focused: classes.filledFocused,
+          disabled: classes.filledDisabled
+        }
+      }}
     />
   )
 }
 
 TextField.propTypes = {
-  children: PropTypes.node.isRequired,
   color: PropTypes.oneOf(['primary', 'secondary']),
-  disabled: PropTypes.boolean,
   variant: PropTypes.oneOf(['filled', 'outlined']),
 }
 
 TextField.defaultProps = {
   color: 'primary',
-  disabled: false,
-  variant: 'round'
+  variant: 'filled'
 };
 
 
