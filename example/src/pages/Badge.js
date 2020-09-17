d41554954977c39d4bda5f45ed6801ce0dee1012
Monica Mota Ocariz <mota.ocariz@gmail.com>
2020-09-17 15:29:34 -0300

    [DSBL-81] Created variantion without link

--- example/src/pages/Badge.js
+++ example/src/pages/Badge.js
@@ -11,9 +11,54 @@ const App = () => {
             <Text variant='h4' component='h2' gutterBottom>
               Badge
             </Text>
-            <Grid item xs={6} style={{ marginTop: '5%' }}>
-            </Grid>
           </Grid>
+          <Grid item xs={12}>
+            <Text variant='h6' component='h2' gutterBottom>
+              Normal
+            </Text>
+          </Grid>
+          <Grid item xs={2}>
+            <Badge label="primary" />
+          </Grid>
+          <Grid item xs={2}>
+            <Badge label="Secondary" color="secondary" />
+          </Grid>
+          <Grid item xs={2}>
+            <Badge label="Tertiary" color="tertiary" />
+          </Grid>
+          <Grid item xs={2}>
+            <Badge label="Success" color="success" />
+          </Grid>
+          <Grid item xs={2}>
+            <Badge label="Error" color="error" />
+          </Grid>
+          <Grid item xs={2}>
+            <Badge label="Warning" color="warning" />
+          </Grid>
+
+          <Grid item xs={12}>
+              <Text variant='h6' component='h2' gutterBottom>
+                Pill
+              </Text>
+            </Grid>
+            <Grid item xs={2}>
+              <Badge label="primary" variant="pill" />
+            </Grid>
+            <Grid item xs={2}>
+              <Badge label="Secondary" variant="pill" color="secondary" />
+            </Grid>
+            <Grid item xs={2}>
+              <Badge label="Tertiary" variant="pill" color="tertiary" />
+            </Grid>
+            <Grid item xs={2}>
+              <Badge label="Success" variant="pill" color="success" />
+            </Grid>
+            <Grid item xs={2}>
+              <Badge label="Error" variant="pill" color="error" />
+            </Grid>
+            <Grid item xs={2}>
+              <Badge label="Warning" variant="pill" color="warning" />
+            </Grid>
         </Grid>
       </Container>
     </div>
--- src/lib/Badge/index.js
+++ src/lib/Badge/index.js
@@ -1,17 +1,53 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Layout from '../Layout';
+import Text from '../Text';
+import { makeStyles, fade } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  badge: (props) => ({
+    backgroundColor: theme.palette[props.color]
+      ? theme.palette[props.color].main:
+      theme.palette.primary.main,
+    color: theme.palette[props.color]
+      ? theme.palette[props.color].contrastText:
+      theme.palette.primary.contrastText,
+    borderRadius: props.variant === 'normal'
+      ? 4 : 14,
+    boxShadow: theme.shadows[1],
+    padding: '2px 8px 4px 8px',
+  }),
+  textRoot: {
+    fontSize: theme.typography.pxToRem(12),
+    lineHeight: theme.typography.pxToRem(20),
+    letterSpacing: '0.004em',
+  }
+}));
 
 const Badge = (props) => {
+  const classes = useStyles({
+    color: props.color,
+    variant: props.variant
+  });
+
   return (
-    <div>
-      oi
-    </div>
+    <span className={classes.badge}>
+      <Text variant='caption' classes={{ root: classes.textRoot }}>
+        {props.label}
+      </Text>
+    </span>
   )
 };
 
 Badge.propTypes = {
-  children: PropTypes.node.isRequired
+  label: PropTypes.string.isRequired,
+  color: PropTypes.oneOf(['primary', 'secondary', 'tertiary', 'success', 'error', 'warning']),
+  variant: PropTypes.oneOf(['normal', 'pill'])
+};
+
+Badge.defaultProps ={
+  color: 'primary',
+  variant: 'normal'
 };
 
 export default (props) => (
